de8ea27469d357a0a27ecf1ca2689fac800294cf
Vedansh7-7 <adm.shajapur1@gmail.com>
2026-08-15 21:32:50 +0530

    dataflowDB initiated

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+def initiate():
+    load_dotenv()
+    print("Initiating main-DB:\n")
+    connection = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = os.getenv("pass")
+    )
+    print("Connected !")
+
+    cursor = connection.cursor()
+    cursor.execute("CREATE DATABASE IF NOT EXISTS dataflowDB")
+    print("Database initiated !")
+
+    return cursor, connection
+
+def terminate(cursor, connection):
+    cursor.close()
+    connection.close()
+
+
 def main():
+    
     print("Hello from dataflow!")
+    cursor, connection = initiate()
+    terminate(cursor, connection)
+    
 
 
 if __name__ == "__main__":
